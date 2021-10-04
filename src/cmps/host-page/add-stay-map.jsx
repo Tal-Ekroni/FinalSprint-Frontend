841e113a764cc6ac2fcc294f59e12129be57e9b4
IdoElgarisi <ido12el@gmail.com>
2021-10-04 15:46:45 +0300

    ido add stay almost - done address-bug

--- src/cmps/host-page/add-stay-map.jsx
+++ src/cmps/host-page/add-stay-map.jsx
@@ -63,41 +63,11 @@ class _AddStayMap extends Component {
                     onClick={this.onMapClicked}
                     center={this.state.center}
                 >
-                    {/* <StandaloneSearchBox>
-                        <input
-                            type="text"
-                            placeholder="Customized your placeholder"
-                            style={{
-                                boxSizing: `border-box`,
-                                border: `1px solid transparent`,
-                                width: `240px`,
-                                height: `32px`,
-                                padding: `0 12px`,
-                                borderRadius: `3px`,
-                                boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-                                fontSize: `14px`,
-                                outline: `none`,
-                                textOverflow: `ellipses`,
-                                position: "absolute",
-                                left: "50%",
-                                marginLeft: "-120px"
-                            }}
-                        />
-                    </StandaloneSearchBox> */}
                     <Marker
                         position={activeMarker}
                         name={'Current location'}
                         onClick={this.onMarkerClicked}
                     />
-                    {/* <Marker
-                            key={loc.name}
-                            position={loc}
-                            name={'Current location'}
-                            onClick={this.onMarkerClicked}
-                        /> */}
-
-
-
                     <InfoWindow
                         onClose={this.onInfoWindowClose}
                         position={this.state.center}
