a1b77ed103bc0fd37b9bd235797069a889ce4ef4
IdoElgarisi <ido12el@gmail.com>
2021-10-07 15:09:03 +0300

    ido push fix bug

--- src/cmps/app-footer.jsx
+++ src/cmps/app-footer.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 import { TopRatedStays } from './app-footer/top-rated-stays'
 import { loadStays } from '../store/stay.actions'
 import { removeFromCart, checkout } from '../store/stay.actions'
-import { onBookTrip } from '../store/user.actions'
+import { onBookTrip, loadUser } from '../store/user.actions'
 
 import { UserMsg } from './user-msg.jsx'
 
@@ -18,6 +18,7 @@ class _AppFooter extends React.Component {
 
     async componentDidMount() {
         await this.props.loadStays(this.props.filterBy)
+        await this.props.loadUser()
         const topRatedStays = this.props.stays.slice(1)
         console.log(topRatedStays)
         this.setState({ topRatedStays })
@@ -30,7 +31,7 @@ class _AppFooter extends React.Component {
     checkout = () => {
         this.props.checkout();
     }
-    get staytTotal() {
+    getstaytTotal() {
         return this.props.cart.reduce((acc, stay) => acc + stay.price, 0)
     }
 
@@ -41,14 +42,14 @@ class _AppFooter extends React.Component {
                 <h2>Insparation for future getaways</h2>
                 <div className="flex column">
                     <h4>Top rated stays</h4>
-                    <TopRatedStays  stays={this.state.topRatedStays} />
+                    <TopRatedStays stays={this.state.topRatedStays} />
                 </div>
                 <div className="footer-info flex justify-start align-center">
                     <p>
                         © 2021 AnyGo ,Inc.,
                     </p>
                     <Link to='/login'>· Login</Link>
-                    <Link to={user.isHost ? '/host' : '/become-a-host'}>{user.isHost ? '· Host page' : '· Become a host'}</Link>
+                    <Link to={user && user.isHost ? '/host' : '/become-a-host'}>{user && user.isHost ? '· Host page' : '· Become a host'}</Link>
                 </div>
                 <UserMsg />
             </footer>
@@ -71,7 +72,8 @@ const mapDispatchToProps = {
     checkout,
     onBookTrip,
     removeFromCart,
-    loadStays
+    loadStays,
+    loadUser    
 }
 
 export const AppFooter = connect(mapStateToProps, mapDispatchToProps)(_AppFooter)
